845dc65a32344070df76a6885bcad90db6e55486
MaulanaMalikS <67823073+MaulanaMalikS@users.noreply.github.com>
2021-12-16 14:57:57 +0700

    Mengubah path model

--- drug_classsifier_deploy.py
+++ drug_classsifier_deploy.py
@@ -6,8 +6,8 @@ import streamlit as st
 import pickle
 
 # Memuat model drug_classifier
-with open('drug_classifier.pkl', 'rb') as file:
-    model = pickle.load(file)
+with open('/app/drug_classifier/drug_classifier.pkl', 'rb') as file:
+    model = pickle.load(file) #cek
 
 # Tampilan judul dan kata pengantar
 st.write("""
