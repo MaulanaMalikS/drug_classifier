ef2495f0750edfb33a6a9d72ef903f3efea6dedc
MaulanaMalikS <67823073+MaulanaMalikS@users.noreply.github.com>
2021-12-16 14:45:58 +0700

    Upload Project

---
+++ drug_classsifier_deploy.py
@@ -0,0 +1,52 @@
+# MEMUAT MODEL
+
+# Import library streamlit
+import streamlit as st
+# Import library pickle file ekstensi .pkl
+import pickle
+
+# Memuat model drug_classifier
+with open('/Data/Pemrograman/Python/ Artificial Intelligence/Latihan/Model/drug_classifier.pkl', 'rb') as file:
+    model = pickle.load(file)
+
+# Tampilan judul dan kata pengantar
+st.write("""
+    # Drug Classifier App
+
+    Memberikan obat yang tepat untuk pasien dengan data Umur, Jenis Kelamin, Tekanan Darah, Kolesterol,  Rasio Sodium dan Potasium dalam Darah
+""")
+
+# form input user
+st.write("""
+    ##### Masukkan data pasien
+""")
+umur = st.number_input('Umur', 0, value=25)
+kelamin = st.radio('Jenis Kelamin', ['Laki-laki', 'Perempuan'])
+darah = st.radio('Tekanan Darah', ['Rendah', 'Normal', 'Tinggi'])
+kol = st.radio('Kolesterol', ['Normal', 'Tinggi'])
+Na_to_K = st.number_input('Rasio Sodium dan Potasium dalam Darah', 0, value=15)
+
+# Encoding Jenis Kelamin
+if kelamin == 'Laki-laki' :
+    kelamin = 1
+else:
+    kelamin = 0
+
+# Encoding Tekanan Darah
+if darah == 'Rendah' :
+    darah = 1
+elif darah == 'Normal' :
+    darah = 2
+else:
+    darah = 3
+
+# Encoding Kolesterol
+if kol == 'Normal' :
+    kol = 1
+else:
+    kol = 0
+
+st.write("""
+    ##### Prediksi
+    Obat : 
+""", '**', model.predict([[umur, kelamin, darah, kol, Na_to_K]])[0], '**')
